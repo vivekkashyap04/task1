c9f344c3e73755f9e4fec5859e0654c3637cf4a6
vivekkashyap04 <31179856+vivekkashyap04@users.noreply.github.com>
2018-03-03 15:01:27 +0530

    constructor

---
+++ index.js
@@ -0,0 +1,8 @@
+function Person(name,age,rollNo,section){
+		this.name=name;
+		this.age=age;
+		this.rollNo=rollNo;
+		this.section=section;
+	}
+var person1=new Person("vivek",23,12,"a");
+document.getElementById("demo").person1;	
